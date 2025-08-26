cb2ab79894e2f74c485348441b2cc7c4e1e7be5e
xgd <895740313@qq.com>
2025-08-26 11:13:02 +0800

    feat(chat): 添加数据库类型动态获取功能
    从数据库配置中动态获取数据库类型，替换硬编码的"mysql"类型

--- internal/logic/ai/chat.go
+++ internal/logic/ai/chat.go
@@ -2,15 +2,19 @@ package ai
 
 import (
 	"ai-chat-sql/internal/consts"
+	"ai-chat-sql/internal/dao"
 	"ai-chat-sql/internal/model"
 	"ai-chat-sql/internal/service"
 	"context"
+	"time"
 
 	"github.com/cloudwego/eino-ext/components/tool/mcp"
 	"github.com/cloudwego/eino/compose"
 	"github.com/cloudwego/eino/flow/agent/react"
 	"github.com/cloudwego/eino/schema"
+	"github.com/gogf/gf/v2/database/gdb"
 	"github.com/gogf/gf/v2/frame/g"
+	"github.com/gogf/gf/v2/util/gconv"
 )
 
 type sAiChat struct{}
@@ -43,6 +47,14 @@ func (s *sAiChat) Chat(ctx context.Context, in model.ChatInput) (respChan chan a
 	if err != nil {
 		return
 	}
+	// 获取需要操作的数据库信息
+	dbTypeT, err := dao.DatabaseConf.Ctx(ctx).Cache(gdb.CacheOption{
+		Duration: 30 * time.Minute,
+		Name:     "db_type:" + gconv.String(in.DatabaseId),
+	}).Where("database_id = ?", in.DatabaseId).Value("db_type")
+	if err != nil {
+		return
+	}
 
 	prompt, err := service.Prompt().GetPrompt(ctx, consts.PromptMain)
 	if err != nil {
@@ -54,7 +66,7 @@ func (s *sAiChat) Chat(ctx context.Context, in model.ChatInput) (respChan chan a
 	out, err := aiAgent.Stream(ctx, []*schema.Message{
 		{
 			Role:    schema.System,
-			Content: prompt.GetContent(in.DatabaseId, "mysql"),
+			Content: prompt.GetContent(in.DatabaseId, dbTypeT.String()),
 		},
 		{
 			Role:    schema.System,
